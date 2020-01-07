c6a43f454204434b8df253f805c164ecc40a6a01
Rui Barcelos <barcelos.rui@gmail.com>
2020-01-07 22:36:29 +0000

    5 - FEATURE TLS(DON'T WORK) AND CREATE README

--- src/main/java/pt/ipleiria/src/iot/mqttclient/business/MqttClientIot.java
+++ src/main/java/pt/ipleiria/src/iot/mqttclient/business/MqttClientIot.java
@@ -1,9 +1,14 @@
 package pt.ipleiria.src.iot.mqttclient.business;
 
-import org.fusesource.hawtbuf.Buffer;
 import org.fusesource.mqtt.client.*;
 
-import java.net.URISyntaxException;
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 public class MqttClientIot {
 
@@ -15,11 +20,15 @@ public class MqttClientIot {
         connection = mqtt.blockingConnection();
     }
 
-    public MqttClientIot(String serverURI, String userName, String password) throws Exception {
-
+    public MqttClientIot(String serverURI, String userName, String password, boolean isSecure) throws Exception {
         mqtt.setUserName(userName);
         mqtt.setPassword(password);
         mqtt.setHost(serverURI);
+
+        if (isSecure) {
+            SSLContext sslContext = sslContext();
+            mqtt.setSslContext(sslContext);
+        }
         connection = mqtt.blockingConnection();
 
     }
@@ -41,4 +50,26 @@ public class MqttClientIot {
         return new String(message.getPayload());
     }
 
+    private SSLContext sslContext() throws GeneralSecurityException, IOException {
+        InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream("/ca.crt");
+        if (in == null) {
+            return null;
+        }
+
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509Certificate caCert = (X509Certificate) cf.generateCertificate(in);
+
+        TrustManagerFactory tmf = TrustManagerFactory
+                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(null); // You don't need the KeyStore instance to come from a file.
+        ks.setCertificateEntry("caCert", caCert);
+
+        tmf.init(ks);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+
+        return sslContext;
+    }
 }
--- src/main/java/pt/ipleiria/src/iot/mqttclient/controller/AlgorithmTlsController.java
+++ src/main/java/pt/ipleiria/src/iot/mqttclient/controller/AlgorithmTlsController.java
@@ -9,30 +9,29 @@ import pt.ipleiria.src.iot.mqttclient.business.*;
 import java.nio.charset.StandardCharsets;
 
 @RestController
-@RequestMapping("/api/algorithm/auth")
-public class AlgorithmAuthController {
-    private MqttClientIot mqttClientIot = new MqttClientIot("tcp://localhost:1883", "mqtt", "mqtt", false);
+@RequestMapping("/api/algorithm/tls")
+public class AlgorithmTlsController {
+    private MqttClientIot mqttClientIot = new MqttClientIot("ssl://localhost:8883", "mqtt", "mqtt", true);
 
     private static String topic = "alert";
 
-    public AlgorithmAuthController() throws Exception {
-
+    public AlgorithmTlsController() throws Exception {
     }
 
     @GetMapping("/plain")
-    public ResponseEntity<String> getPlainAuth() throws Exception {
+    public ResponseEntity<String> getPlainTls() throws Exception {
         return ResponseEntity.ok(mqttClientIot.subscribe(topic));
     }
 
     @GetMapping("/aes128ecb")
-    public ResponseEntity<String> getAes128EcbAuth() throws Exception {
+    public ResponseEntity<String> getAes128EcbTls() throws Exception {
         byte[] cipher = IotUtils.asBytes(mqttClientIot.subscribe(topic));
         String myKey = "abcdefghijklmnop";
         return ResponseEntity.ok(new String(Aes128Ecb.decryptMessage(cipher, myKey.getBytes(StandardCharsets.UTF_8))));
     }
 
     @GetMapping("/3desecb")
-    public ResponseEntity<String> get3DesAuth() throws Exception {
+    public ResponseEntity<String> get3DesTls() throws Exception {
         String myKey = "abcdefghijklmnop12345678";
         byte[] cipher = IotUtils.asBytes(mqttClientIot.subscribe(topic));
 
@@ -40,7 +39,7 @@ public class AlgorithmAuthController {
     }
 
     @GetMapping("/desecb")
-    public ResponseEntity<String> getDesAuth() throws Exception {
+    public ResponseEntity<String> getDesTls() throws Exception {
         String myKey = "abcdefgh";
         byte[] cipher = IotUtils.asBytes(mqttClientIot.subscribe(topic));
 
